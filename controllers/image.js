8d9894c001ea09f6574acfc26ac4e5567694be0d
lucasbertolo <lucasbertolo2@gmail.com>
2018-07-23 13:50:26 -0300

    adding project

---
+++ controllers/image.js
@@ -0,0 +1,25 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'c7d28a10d20a4ed98bfa0dc3c0d4348c'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data => res.json(data))
+	.catch(err => res.status(400).json('unable to work with api'))	
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=' , id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0])
+		})
+		.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = { handleImage }
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'lucasbertolo',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
+
+app.put('/image', (req, res) => image.handleImage(req, res, db));
+
+app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res)});
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{
+	console.log(`server is listening on port ${PORT}`);
+});
