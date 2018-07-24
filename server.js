927754133e723a74b230426f7e1478d28d9ad424
lucasbertolo <lucasbertolo2@gmail.com>
2018-07-24 01:33:34 -0300

    updating database

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image = require('./controllers/image');
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'lucasbertolo',
-    password : '',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+   	ssl: true
   }
 });
 
